b9f15a9e8f6226ca6ec808fba3d9deea582cec0e
Emily Cheng <echeng0123@gmail.com>
2023-09-14 00:00:42 -0400

    nonworking chunking code

--- src/API/fetching.js
+++ src/API/fetching.js
@@ -26,20 +26,59 @@ export const fetchSingleSpell = async (spellUrl) => {
 
 // JOIN SPELLS
 // Because the D&D 5e API is not set up to grab spells with full info, this function creates a new array that pulls in the full spell list by grabbing each spell individually.
+// export async function joinSpells() {
+// 	console.log("Entering joinSpells");
+
+// 	try {
+// 		const APIResponse = await fetchAllSpellsRaw();
+// 		const cleanSpells = [];
+// 		const APIlength = Object.keys(APIResponse.results).length;
+
+// 		for (let i = 0; i < APIlength; i++) {
+// 			let singleSpell = await fetchSingleSpell(APIResponse.results[i].index);
+// 			cleanSpells.push(singleSpell);
+// 		}
+// 		console.log("cleanSpells", cleanSpells);
+// 		return cleanSpells;
+// 	} catch (error) {
+// 		console.error("Can't fetch all raw spells", error);
+// 	}
+// }
+
 export async function joinSpells() {
 	console.log("Entering joinSpells");
-	try {
+	const chunk = 50;
+	let index = 0;
+
+	async function doChunk() {
+		console.log("entering doChunk");
+		let count = chunk;
 		const APIResponse = await fetchAllSpellsRaw();
 		const cleanSpells = [];
-		const APIlength = Object.keys(APIResponse.results).length;
+		// const APIlength = Object.keys(APIResponse.results).length;
+		const APIlength = 50;
 
-		for (let i = 0; i < APIlength; i++) {
-			let singleSpell = await fetchSingleSpell(APIResponse.results[i].index);
-			cleanSpells.push(singleSpell);
+		while (count-- && index < APIlength) {
+			console.log("hello", count);
+			for (let i = 0; i < APIlength; i++) {
+				let singleSpell = await fetchSingleSpell(APIResponse.results[i].index);
+				cleanSpells.push(singleSpell);
+			}
+			++index;
+		}
+		if (index < APIlength) {
+			// set timeout for async iteration
+			setTimeout(doChunk, 1);
 		}
 		console.log("cleanSpells", cleanSpells);
 		return cleanSpells;
-	} catch (error) {
-		console.error("Can't fetch all raw spells", error);
 	}
+	await doChunk();
+
+	// console.log("cleanSpells", cleanSpells);
+	// return cleanSpells;
+	// try {
+	// } catch (error) {
+	// 	console.error("Can't fetch all raw spells", error);
+	// }
 }
